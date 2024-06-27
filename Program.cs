8f4e206bb7e9e9925830b642c3ee975d8a8fe70b
MidKnightXI <romain.bouguet@epitech.eu>
2024-06-27 10:20:55 +0200

    feat: improving naming on variables

--- Program.cs
+++ Program.cs
@@ -5,37 +5,40 @@ internal static class Program
 {
     private static int Main(string[] args)
     {
-        var rootCommand = new RootCommand("MetaDate - Date detector");
-
+        var rootCommand = new RootCommand("MetaDate - Media ordering by dates");
         var target = new Argument<DirectoryInfo>(
             name: "target",
             description: "The directory containing the images to analyze.")
         {
             Arity = ArgumentArity.ExactlyOne
         };
-
         var output = new Option<DirectoryInfo>(
             name: "--output",
-            description: "The output file path for the prediction results",
-            getDefaultValue: () => new DirectoryInfo("./"));
-
-        var dateOption = new Option<string>(
+            description: "Where to copy the files",
+            getDefaultValue: () => new DirectoryInfo("./"))
+        {
+            Arity = ArgumentArity.ExactlyOne,
+        };
+        var orderByOption = new Option<string>(
             name: "--orderBy",
             description: "Specify the type of date information to process: 'day', 'month', or 'year'",
-            getDefaultValue: () => "day");
+            getDefaultValue: () => "day")
+        {
+            Arity = ArgumentArity.ExactlyOne,
+        };
 
-        dateOption.AddValidator(result =>
+        orderByOption.AddValidator(result =>
         {
             var value = result.GetValueOrDefault<string>();
             if (value is not "day" && value is not "month" && value is not "year")
             {
-                result.ErrorMessage = "The value for --date must be 'day', 'month', or 'year'.";
+                result.ErrorMessage = "The value for --orderBy must be 'day', 'month', or 'year'.";
             }
         });
 
         rootCommand.AddArgument(target);
         rootCommand.AddOption(output);
-        rootCommand.AddOption(dateOption);
+        rootCommand.AddOption(orderByOption);
 
         rootCommand.SetHandler((DirectoryInfo targ, DirectoryInfo outputPath, string date) =>
         {
@@ -45,7 +48,7 @@ internal static class Program
             }
             var results = InfosExtractor.RetrieveInformation(targ);
             InfosExtractor.OrderPictures(results, date, outputPath);
-        }, target, output, dateOption);
+        }, target, output, orderByOption);
 
         return rootCommand.Invoke(args);
     }
